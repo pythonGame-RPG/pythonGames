c892b4ca6ee4645dcb3ae4ece60b01f0c5fede66
Giphe <yama8ky7@gmail.com>
2020-05-27 00:51:05 +0900

    ５月２７日スクレイピング成功

--- Src/py_sample/resouces/Display/getname.py
+++ Src/py_sample/resouces/Display/getname.py
@@ -6,9 +6,11 @@ logger.setLevel(DEBUG)
 logger.addHandler(handler)
 logger.propagate = False
 import os
+import re
 import time
 import datetime
 import csv
+import random
 from urllib.request import urlopen
 # import requests
 from bs4 import BeautifulSoup
@@ -20,9 +22,15 @@ def main():
     # --------------------------------------
     # 作品ページのURLを指定（コメントアウト・コメントインで指定できるようにしています）
     url_list = [
-        "https://ncode.syosetu.com/n2267be/" # Ｒｅ：ゼロから始める異世界生活
+        "https://yomou.syosetu.com/rank/list/type/daily_total/" # 小説を読もう日間ランキング
         ,
-        "https://ncode.syosetu.com/n6316bn/" # 転生したらスライムだった件
+        "https://yomou.syosetu.com/rank/list/type/weekly_total/" # 小説を読もう週刊ランキング
+        ,
+        "https://yomou.syosetu.com/rank/list/type/monthly_total/" # 小説を読もう月間ランキング
+    #    ,
+    #    "https://ncode.syosetu.com/n2267be/" # Ｒｅ：ゼロから始める異世界生活
+    #    ,
+    #    "https://ncode.syosetu.com/n6316bn/" # 転生したらスライムだった件
     #     ,
     #     "https://ncode.syosetu.com/n2031cu/" # 異世界転移で女神様から祝福を！　～いえ、手持ちの異能があるので結構です～
     #     ,
@@ -33,26 +41,46 @@ def main():
     #     "https://ncode.syosetu.com/n5881cl/" # 賢者の孫
                ]
     # --------------------------------------
-    # 各作品について処理
-    for url in url_list:
-        stories = []
+    # ランキングから開始
+    stories = []
+    name_list = []
+    url = random.choice(url_list)
+    bs_obj = make_bs_obj(url)
+    time.sleep(3)
+
+    # TODO：ranking_listとどちらが早いかを検証する
+    #url_list = ["https://ncode.syosetu.com" + a_bs_obj.find("a").attrs["href"] for a_bs_obj in bs_obj.findAll("div", {"class": "rank_h"})]
+    #url_list = [a_bs_obj.find("a").attrs["href"] for a_bs_obj in bs_obj.findAll("div", {"class": "rank_h"})]
+    # date_list = bs_obj.findAll("dt",{"class":"long_update"})
+    # novel_title = bs_obj.find("p",{"class":"novel_title"}).get_text()
+    #for s in r'\/*?"<>:|':
+    #    novel_title = novel_title.replace(s, '')
+
+    # 各話の本文情報を取得
+
+    # 小説を取得
+    novel_list = [a_bs_obj.find("a").attrs["href"] for a_bs_obj in bs_obj.findAll("div", {"class": "rank_h"})]
+    time.sleep(3)
+
+    rand_num = random.choices(range(len(novel_list)), k=10)
+    for i in rand_num:
+
+        url = novel_list[i]
         bs_obj = make_bs_obj(url)
-        time.sleep(3)
+        # ストーリーを取得
+        story_list = ["https://ncode.syosetu.com" + a_bs_obj.find("a").attrs["href"] for a_bs_obj in bs_obj.findAll("dd", {"class": "subtitle"})]
 
-        url_list = ["https://ncode.syosetu.com" + a_bs_obj.find("a").attrs["href"] for a_bs_obj in bs_obj.findAll("dl", {"class": "novel_sublist2"})]
-        date_list = bs_obj.findAll("dt",{"class":"long_update"})
-        novel_title = bs_obj.find("p",{"class":"novel_title"}).get_text()
-        for s in r'\/*?"<>:|':
-            novel_title = novel_title.replace(s, '')
-
-        # 各話の本文情報を取得
-        for j in range(len(url_list)):
-            if j == 3:
-                break
-            url = url_list[j]
+        lim_num = 10
+        story_num = len(story_list)
+        if story_num < 10:
+            lim_num = story_num
+        rand_num = random.choices(range(len(story_list)), k=lim_num)
+    
+        for l in rand_num:
+            url = story_list[l]
             bs_obj = make_bs_obj(url)
-            time.sleep(3)
-
+            nov_text = get_main_text(bs_obj)
+            """
             stories.append({
                 "No": j+1,
                 "title": bs_obj.find("p", {"class": "novel_subtitle"}).get_text(),
@@ -60,8 +88,14 @@ def main():
                 "date": date_list[j].get_text(),
                 "text": get_main_text(bs_obj),
                 })
+            """
+            story_name = re.findall('[\u30A1-\u30FF]{2,10}',nov_text
+            name_list.append(story_name)
 
-        save_as_csv(stories, novel_title)
+            print(story_name)
+
+    insert_name(name_list)
+    #save_as_csv(stories, novel_title)
 
 def make_bs_obj(url):
     """
@@ -69,10 +103,11 @@ def make_bs_obj(url):
     """
     html = urlopen(url)
     # html = requests.get(url)
-    content_type_encoding = html.encoding if html.encoding != 'ISO-8859-1' else None
+    # content_type_encoding = html.encoding if html.encoding != 'ISO-8859-1' else None
     logger.debug('access {} ...'.format(url))
 
-    return BeautifulSoup(html,"html.parser",from_encoding=content_type_encoding)
+    # return BeautifulSoup(html,"html.parser",from_encoding=content_type_encoding)
+    return BeautifulSoup(html,"html.parser")
 
 def get_main_text(bs_obj):
     """
